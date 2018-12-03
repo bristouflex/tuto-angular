874714ee569b01062258a443ea42c93451a75da2
bristouflex <b.hounza@laposte.net>
2018-12-03 13:13:41 +0100

    continuer le travail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   isAuth: boolean = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
+    {status: 'allumé', name: 'Machine à laver'},
+    {status: 'éteint', name: 'Frigo'},
+    {status: 'éteint', name: 'Ordinateur'}
+  ]
+  constructor(){
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer(){
+    //do something
+  }
 }
+
